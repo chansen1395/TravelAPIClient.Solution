9f7b4553ee08076ee41129a3364f5fa54341ecc9
Connor Hansen <chansen13@georgefox.edu>
2021-10-28 09:54:38 -0700

    add CRUD, clean up integration with API.
    Co-authored-by: Art Weinstein <artur.weinstein@gmail.com>

--- APICommunicator/Controllers/ReviewsController.cs
+++ APICommunicator/Controllers/ReviewsController.cs
@@ -20,34 +20,36 @@ namespace TravelAPIClient.Controllers
     [HttpPost]
     public IActionResult Index(Review review)
     {
+      System.Console.WriteLine("Username: " + review.User);
+      System.Console.WriteLine("Country: " + review.Country);
       Review.Post(review);
       return RedirectToAction("Index");
     }
 
-    // public IActionResult Details(int id)
-    // {
-    //   var review = Review.GetDetails(id);
-    //   return View(review);
-    // }
+    public IActionResult Details(int id)
+    {
+      var review = Review.GetDetails(id);
+      return View(review);
+    }
 
-    // public IActionResult Edit(int id)
-    // {
-    //   var review = Review.GetDetails(id);
-    //   return View(review);
-    // }
+    public IActionResult Edit(int id)
+    {
+      var review = Review.GetDetails(id);
+      return View(review);
+    }
 
-    // [HttpPost]
-    // public IActionResult Details(int id, Review review)
-    // {
-    //   review.ReviewId = id;
-    //   Review.Put(review);
-    //   return RedirectToAction("Details", id);
-    // }
+    [HttpPost]
+    public IActionResult Details(int id, Review review)
+    {
+      review.ReviewId = id;
+      Review.Put(review);
+      return RedirectToAction("Details", id);
+    }
 
-    // public IActionResult Delete(int id)
-    // {
-    //   Review.Delete(id);
-    //   return RedirectToAction("Index");
-    // }
+    public IActionResult Delete(int id)
+    {
+      Review.Delete(id);
+      return RedirectToAction("Index");
+    }
   }
 }
